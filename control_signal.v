1b7e45f0475f091911a8259ca13a5e9e2df77a86
bettercallhunter <113797086+bettercallhunter@users.noreply.github.com>
2022-11-03 10:47:36 -0400

    add control signal(tested)

--- control_signal.v
+++ control_signal.v
@@ -1,28 +1,25 @@
-module control_signal(BR, JP, ALUinB, ALUop, DMwe, Rwe,Rdst, Rwd,opcode,aluopcode)
-    output BR,JP,ALUinB, DMwe,Rwe,Rdst,Rwd;
-    output [4:0]ALUop;
+module control_signal(ALUinB, DMwe, Rwe,Rdst, Rwd,opcode);
+    output ALUinB, DMwe,Rwe,Rdst,Rwd;
+
     input [4:0]opcode;
-    input [4:0]aluopcode
-    wire [31:0]extendop
-    wire [31:0]extendaluop
-    wire in_add,in_addi,in_sub,in_or,in_sll,in_sra,in_sw,in_lw;
+ 
+    wire [31:0]extendop;
+    wire [31:0]extendaluop;
+    wire in_addi,in_sw,in_lw;
     five_to_thirtytwo_decoder(extendop,opcode);
     five_to_thirtytwo_decoder(extendaluop,aluopcode);
     //assign instructions
-    and and1(in_add,extendop[0],extendaluop[0]);
+
     assign in_addi = extendop[5];
-    and and2(in_sub,extendop[0],extendaluop[1]);
-    and and3(in_or,extendop[0],extendaluop[3]);
-    and and4(in_sll,extendop[0],extendaluop[4]);
-    and and5(in_sra,extendop[0],extendaluop[5]);
+
     assign in_sw = extendop[7];
     assign in_lw = extendop[8];
     //assign control unit
     or or1(ALUinB,in_addi,in_lw,in_sw);
     assign DMwe = in_sw;
-    or or2(Rwe,in_add,in_addi,in_lw);
-    assign Rdst=in_add
-    assign Rwd =lw
+    or or2(Rwe,extendop[0],in_addi,in_lw);
+    assign Rdst=extendop[0];
+    assign Rwd =in_lw;
 
     
     
