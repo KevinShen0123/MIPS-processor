b17d2cc9dd5788984e7f80b56c71818ecc72fd8c
bettercallhunter <huntershen10@gmail.com>
2022-11-03 15:23:52 -0400

    change pc dffe

--- pc.v
+++ pc.v
@@ -7,7 +7,7 @@ module pc(pc_out, clock, reset, pc_in);
 	genvar i;
 	generate  
 	for(i = 0; i < 32; i = i + 1) begin:pc
-		dffe pc_dffe(pc_out[i], pc_in[i], clock, 1'b1, reset);
+		dffe_ref pc_dffe(pc_out[i], pc_in[i], clock, 1'b1, reset);
 	end 
 	endgenerate
 endmodule
