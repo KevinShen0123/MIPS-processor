3e65b74e9aefadeba0a7f532d3fdc4f69fc79417
bettercallhunter <113797086+bettercallhunter@users.noreply.github.com>
2022-11-27 15:15:36 -0500

    Update skeleton_test.v

--- skeleton_test.v
+++ skeleton_test.v
@@ -11,7 +11,6 @@
 
 module skeleton_test(clock, reset, imem_clock, dmem_clock, processor_clock, regfile_clock,
 address_imem,q_imem,address_dmem,data,wren,q_dmem,ctrl_writeEnable,ctrl_writeReg,ctrl_readRegA, ctrl_readRegB,
-data_writeReg,data_readRegA, data_readRegB,data_reg_write,aluinput, alu_opcode,sximmed,data_writeTwo,enableTwo,overflow);
 data_writeReg,data_readRegA, data_readRegB,data_reg_write,aluinput, alu_opcode,sximmed,data_writeTwo,enableTwo,overflow,pc_in);
 	output [31:0]pc_in;
 	output enableTwo;
